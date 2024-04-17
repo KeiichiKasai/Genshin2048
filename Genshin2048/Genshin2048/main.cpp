897162d1199eb0c440f97b47f15bb67158c8c6c3
KeiichiKasai <114374670+KeiichiKasai@users.noreply.github.com>
2024-04-18 00:48:53 +0800

    feat:add move logic

--- Genshin2048/Genshin2048/main.cpp
+++ Genshin2048/Genshin2048/main.cpp
@@ -4,11 +4,33 @@
 #include <conio.h>
 #include <string.h>
 #include <graphics.h>
+#define _CRT_NONSTDC_NO_DEPRECATE
+#define _CRT_SECURE_NO_WARNINGS
 #pragma comment(lib,"winmm.lib")
 #define Width 480
 #define Height 680
 int Data[4][4];//存储格子信息，当Data[i][j]=0时，为空白格
+int Temp[4][4];
 int BestScore, CurrentScore;
+IMAGE img0, img2, img4, img8, img16, img32, img64, img128, img256, img512, img1024, img2048;
+void CreateData();
+void InitGame();
+void InitData();
+int Check();
+int Find2048();
+void TakeNote();
+int CompareNote();
+void Up();
+void Down();
+void Left();
+void Right();
+void Reset();
+void Vectory();
+void Defeat();
+void RefreshImage();
+void Start();
+void Menu();
+
 void InitData() {
 	int i, j;
 	for (i = 0; i < 4; i++) {
@@ -28,6 +50,177 @@ int Check() {
 	}
 	return 1;
 }
+int	Find2048() {
+	int i, j;
+	for (i = 0; i < 4; i++) {
+		for (j = 0; j < 4; j++) {
+			if (Data[i][j])return 1;
+		}
+	}
+	return 0;
+}
+void TakeNote() {
+	int i, j;
+	for (i = 0; i < 4; i++) {
+		for (j = 0; j < 4; j++) {
+			Temp[i][j] = Data[i][j];
+		}
+	}
+}
+int CompareNote() {
+	int i, j;
+	for (i = 0; i < 4; i++) {
+		for (j = 0; j < 4; j++) {
+			if (Temp[i][j] != Data[i][j])return 0;
+		}
+	}
+	return 1;
+}
+void Up() {
+	int i, j, m, step = 0;
+	
+	for (j = 0; j < 4; j++) {
+		i = 0, step = 0;
+		while (i < 3 && step < 3) {
+			if (!Data[i][j]) {
+				for (m = i; m < 3; m++)Data[m][j] = Data[m + 1][j];
+				Data[3][i] = 0;
+				step++;
+			}
+			else i++;
+		}
+		for (i = 0; i < 3; i++) {
+			if (Data[i][j] == Data[i + 1][j] && Data[i][j] != 0) {
+				Data[i][j] = Data[i][j] * 2;
+				Data[j + 1][i] = 0;
+				CurrentScore += Data[i][j];
+				if (CurrentScore > BestScore)BestScore = CurrentScore;
+			}
+			i = 0, step = 0;
+			while (i < 3 && step < 3) {
+				if (!Data[i][j]) {
+					for (m = i; m < 3; m++)Data[m][i] = Data[m + 1][i];
+					Data[3][i] = 0;
+					step++;
+				}
+				else j++;
+			}
+		}
+	}
+	if (!CompareNote()) CreateData();
+}
+void Down() {
+	int i, j, m, step = 0;
+	
+	for (j = 0; j < 4; j++) {
+		i = 3, step = 0;
+		while (i >0 && step < 3) {
+			if (! Data[i][j]) {
+				for (m = i; m > 0; m--) Data[m][j] = Data[m - 1][j];
+				Data[0][j] = 0;
+				step++;
+			}
+			else i--;
+		}
+		for (i = 3; i > 0; i--) {
+			if (Data[i][j] == Data[i - 1][j] && Data[i][j] != 0) {
+				Data[i][j] = Data[i][j] * 2;
+				Data[i-1][j] = 0;
+				CurrentScore += Data[i][j];
+				if (CurrentScore > BestScore)BestScore = CurrentScore;
+			}
+		}
+		i = 3, step = 0;
+		while (i > 3 && step < 3) {
+			if (!Data[i][j]) {
+				for (m = i; m > 0; m--)Data[m][i] = Data[m - 1][i];
+				Data[0][i] = 0;
+				step++;
+			}
+			else j--;
+		}
+	}
+	if (!CompareNote()) CreateData();
+}
+void Left() {
+	int i, j, m, step = 0;
+	
+	for (i = 0; i < 4; i++) {
+		j = 0, step = 0;
+		while (j < 3 && step < 3) {
+			if (!Data[i][j]) {
+				for (m = j; m < 3; m++)Data[i][m] = Data[i][m + 1];
+				Data[i][3] = 0;
+				step++;
+			}
+			else j++;
+		}	
+		for (j = 0; j < 3; j++) {
+			if (Data[i][j] == Data[i][j + 1] && Data[i][j] != 0) {
+				Data[i][j] = Data[i][j] * 2;
+				Data[i][j + 1] = 0;
+				CurrentScore += Data[i][j];
+				if (CurrentScore > BestScore)BestScore = CurrentScore;
+			}
+		}
+		j = 0, step = 0;
+		while (j < 3 && step < 3) {
+			if (!Data[i][j]) {
+				for (m = j; m < 3; m++)Data[i][m] = Data[i][m + 1];
+				Data[i][3] = 0;
+				step++;
+			}
+			else j++;
+		}
+	}
+	if (!CompareNote()) CreateData();
+}
+void Right() {
+	int i, j, m, step = 0;
+	
+	for (i = 0; i < 4; i++) {
+		j = 3, step = 0;
+		while (j > 0 && step < 3) {
+			if (!Data[i][j]) {
+				for (m = j; m >0 ; m--)Data[i][m] = Data[i][m - 1];
+				Data[i][0] = 0;
+				step++;
+			}
+			else j--;
+		}
+		for (j = 3; j >0; j--) {
+			if (Data[i][j] == Data[i][j - 1] && Data[i][j] != 0) {
+				Data[i][j] = Data[i][j] * 2;
+				Data[i][j - 1] = 0;
+				CurrentScore += Data[i][j];
+				if (CurrentScore > BestScore)BestScore = CurrentScore;
+			}
+		}
+		j = 0, step = 0;
+		while (j > 0 && step < 3) {
+			if (!Data[i][j]) {
+				for (m = j; m < 3; m++)Data[i][m] = Data[i][m + 1];
+				Data[i][3] = 0;
+				step++;
+			}
+			else j--;
+		}
+	}
+	if (!CompareNote()) CreateData();
+}
+void Reset() {
+
+}
+void Vectory() {
+	cleardevice();
+	mciSendString("close music", NULL, 0, NULL);
+
+}
+void Defeat() {
+	cleardevice();
+	mciSendString("close music", NULL, 0, NULL);
+	
+}
 void CreateData(){
 	int x, y, choice, data;
 	srand((int)time(0));
@@ -45,13 +238,25 @@ void CreateData(){
 		break;
 	}
 }
-
+void Update() {
+	int kb,i,j,num;
+	EndBatchDraw();
+	kb = _getch();
+	TakeNote();
+	if (kb == 72)Up();
+	else if (kb == 80)Down();
+	else if (kb == 75)Left();
+	else if (kb == 77)Right();
+	else if (kb == 27)Reset();
+	
+}
 void RefreshImage() {
+	int i, j;
 	char CSstr[10],BSstr[10];
 	cleardevice();
 	setfont(100, 0, "Microsoft Yahei UI Bold");
 	setcolor(RGB(217, 250, 244)); //2048 字体
-	outtextxy(230, 20, "2048");
+	outtextxy(15, 0, "2048");
 
 	setfillcolor(RGB(184, 175, 160));//当前得分 和 最高得分 背景 
 
@@ -61,13 +266,13 @@ void RefreshImage() {
 	settextcolor(RGB(248, 247, 241));//当前得分 字体
 	outtextxy(230, 20, "当前得分");
 	setbkcolor(RGB(184, 175, 160));
-	itoa(CurrentScore, CSstr, 10);
+	_itoa(CurrentScore, CSstr, 10);
 	outtextxy(265, 52, CSstr);//分数
 
 	solidroundrect(350, 10, 460, 80, 15, 15);
 	outtextxy(380, 20, "最高得分");
 	setbkcolor(RGB(184, 175, 160));//最高得分 字体背景
-	itoa(BestScore, BSstr, 10);
+	_itoa(BestScore, BSstr, 10);
 	outtextxy(395, 52, BSstr);
 
 	setfillcolor(RGB(141, 122, 105));
@@ -85,16 +290,38 @@ void RefreshImage() {
 	setfillcolor(RGB(187, 173, 160));//里面边框配色
 	solidroundrect(10, 210, 460, 660, 20, 20);
 
-	if(Check())
+	if (Check())Defeat();
+
+	for (i = 0; i < 4; i++) {
+		for (j = 0; j < 4; j++) {
+			switch (Data[i][j]) {
+			case 0:loadimage(&img0, "./image/blank.png", 100, 100); putimage(i * 110 + 20, j * 110 + 220, &img0); break;
+			case 2:loadimage(&img2, "./image/2.png", 100, 100); putimage(i * 110 + 20, j * 110 + 220, &img2); break;
+			case 4:loadimage(&img4, "./image/4.png", 100, 100); putimage(i * 110 + 20, j * 110 + 220, &img4); break;
+			case 8:loadimage(&img8, "./image/8.png", 100, 100); putimage(i * 110 + 20, j * 110 + 220, &img8); break;
+			case 16:loadimage(&img16, "./image/16.png", 100, 100); putimage(i * 110 + 20, j * 110 + 220, &img16); break;
+			case 32:loadimage(&img32, "./image/32.png", 100, 100); putimage(i * 110 + 20, j * 110 + 220, &img32); break;
+			case 64:loadimage(&img64, "./image/64.png", 100, 100); putimage(i * 110 + 20, j * 110 + 220, &img64); break;
+			case 128:loadimage(&img128, "./image/128.png", 100, 100); putimage(i * 110 + 20, j * 110 + 220, &img128); break;
+			case 256:loadimage(&img256, "./image/256.png", 100, 100); putimage(i * 110 + 20, j * 110 + 220, &img256); break;
+			case 512:loadimage(&img512, "./image/512.png", 100, 100); putimage(i * 110 + 20, j * 110 + 220, &img512); break;
+			case 1024:loadimage(&img1024, "./image/1024.png", 100, 100); putimage(i * 110 + 20, j * 110 + 220, &img1024); break;
+			case 2048:loadimage(&img2048, "./image/2048.png", 100, 100); putimage(i * 110 + 20, j * 110 + 220, &img2048); break;
+			}
+		}
+	}
 }
 
 void Start() {
 	InitData();
+	CreateData();
+	CreateData();
 	while (1){
 		RefreshImage();
 		BeginBatchDraw();//批量画图，无闪烁
 		setbkcolor(RGB(251, 248, 241));
-		
+		Update();
+		if(Find2048())Vectory();
 	}
 }
 void Menu()
@@ -118,17 +345,17 @@ void Menu()
 void InitGame()
 {
 	//开场动画
-	mciSendString("open ./bgm/start.mp3 alias music ", NULL, 0, NULL);//背景音乐
-	mciSendString("play music repeat", NULL, 0, NULL);
-	initgraph(1288.8, 724.95);
-	IMAGE s, e;
-	loadimage(&s, "image/genshi.png", 1288.8, 724.95);
-	loadimage(&e, "image/genshin1.png", 1288.8, 724.95);
-	putimage(0, 0, &s);
-	Sleep(2500);
-	putimage(0, 0, &e);
-	_getch();
-	closegraph();
+	//mciSendString("open ./bgm/start.mp3 alias music ", NULL, 0, NULL);//背景音乐
+	//mciSendString("play music repeat", NULL, 0, NULL);
+	//initgraph(1288, 724);
+	//IMAGE s, e;
+	//loadimage(&s, "image/genshi.png", 1288, 724);
+	//loadimage(&e, "image/genshin1.png", 1288, 724);
+	//putimage(0, 0, &s);
+	//Sleep(2500);
+	//putimage(0, 0, &e);
+	//_getch();
+	//closegraph();
 
 
 
